85909ec544b47a2b771181d48171215e027bf5a6
Fernando Lopez <fernandojalope343@gmail.com>
2023-07-30 15:27:38 -0400

    try fix empty or no found

--- src/components/UI/Empty.tsx
+++ src/components/UI/Empty.tsx
@@ -3,31 +3,31 @@ import React from 'react';
 import { Text } from 'react-native-paper';
 import { Body } from './styled';
 
-interface Props{
-  customAnimation: string;
+interface Props {
+  customAnimation?: string;
   text: string;
 }
 
-export const Empty: React.FC<Props> = ({customAnimation, text}) => {
+export const Empty: React.FC<Props> = ({ customAnimation, text }) => {
   return (
     <Body>
       <LottieView
-				autoPlay
-				loop={true}
-				style={{
-					width: "100%",
-					height: 250,
-					backgroundColor: "transparent",
-				}}
-				source={{
-					uri: customAnimation
-						? customAnimation
-						: "https://assets4.lottiefiles.com/private_files/lf30_rrpywigs.json",
-				}}
-			/>
-			<Text variant="titleLarge" style={{ textAlign: "center" }}>
-				{text}
-			</Text>
+        autoPlay
+        loop={true}
+        style={{
+          width: "100%",
+          height: 250,
+          backgroundColor: "transparent",
+        }}
+        source={{
+          uri: customAnimation
+            ? customAnimation
+            : "https://assets4.lottiefiles.com/private_files/lf30_rrpywigs.json",
+        }}
+      />
+      <Text variant="titleLarge" style={{ textAlign: "center" }}>
+        {text}
+      </Text>
     </Body>
   )
 }
--- src/components/UI/NoFouned.tsx
+++ src/components/UI/NoFouned.tsx
@@ -3,28 +3,28 @@ import React from 'react';
 import { Text } from 'react-native-paper';
 import { Body } from './styled';
 
-interface Props{
+interface Props {
   text: string;
 }
 
-export const NoFouned: React.FC<Props> = ({text}) => {
+export const NoFouned: React.FC<Props> = ({ text }) => {
   return (
     <Body>
       <LottieView
-				autoPlay
-				loop={false}
-				style={{
-					width: "100%",
-					height: 250,
-					backgroundColor: "transparent",
-				}}
-				source={{
-					uri: "https://assets9.lottiefiles.com/packages/lf20_dmw3t0vg.json",
-				}}
-			/>
-			<Text variant="titleLarge" style={{ textAlign: "center" }}>
-				{text}
-			</Text>
+        autoPlay
+        loop={false}
+        style={{
+          width: "100%",
+          height: 250,
+          backgroundColor: "transparent",
+        }}
+        source={{
+          uri: "https://assets9.lottiefiles.com/packages/lf20_dmw3t0vg.json",
+        }}
+      />
+      <Text variant="titleLarge" style={{ textAlign: "center" }}>
+        {text}
+      </Text>
     </Body>
   )
 }
