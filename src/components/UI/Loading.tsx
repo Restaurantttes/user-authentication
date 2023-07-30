cda14d2bdaad62cae85b0d39e9522f06d5d9a442
Fernando Lopez <fernandojalope343@gmail.com>
2023-07-30 15:41:37 -0400

    change color loading

--- src/components/UI/Loading.tsx
+++ src/components/UI/Loading.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { ActivityIndicator } from 'react-native'
+import { ActivityIndicator, Platform } from 'react-native'
+import { useTheme } from 'styled-components'
 import { Body } from './styled'
 
 export const Loading = () => {
+  const theme = useTheme()
+
   return (
     <Body>
-      <ActivityIndicator size='large' />
+      <ActivityIndicator size='large' color={Platform.OS === 'android' && theme.colors.primary} />
     </Body>
   )
 }
