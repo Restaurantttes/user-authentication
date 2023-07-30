b458e92ff10353d7eca9b7a4c06d6a33b9174350
Fernando Lopez <fernandojalope343@gmail.com>
2023-07-29 20:11:03 -0400

    import form login

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import { FormLogin } from "./components/FormLogin";
 import { AuthenticationContext, AuthenticationContextProvider } from "./services/context/AuthenticationContext";
 import { getDataAsyncStorage, saveDataAsyncStorage } from "./utils/storage";
 import { useWidth } from "./utils/useWidth";
 
 export {
-  AuthenticationContext, AuthenticationContextProvider, getDataAsyncStorage, saveDataAsyncStorage, useWidth
+  AuthenticationContext, AuthenticationContextProvider, FormLogin, getDataAsyncStorage, saveDataAsyncStorage, useWidth
 };
-
